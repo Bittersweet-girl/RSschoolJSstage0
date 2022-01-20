8b3a626b2ed5df8992b4b85890d6ab21a392469f
Bittersweet-girl <enatav1@gmail.com>
2022-01-20 23:20:16 +0200

    feat: add function closeMenu

--- portfolio/index.js
+++ portfolio/index.js
@@ -8,4 +8,14 @@ function toggleMenu() {
 hamburger.addEventListener('click', toggleMenu);
 headerNav.addEventListener('click', toggleMenu);
 
+const headerLink = document.querySelectorAll('.header__link');
+
+function closeMenu() {
+  if (headerLink.target.classList.contains('header__link')) {
+    hamburger.classList.remove('open');
+    headerNav.classList.remove('open');
+  }
+}
+headerNav.addEventListener('click', closeMenu);
+
 console.log('Оценка 100. \nВёрстка валидная +10, \nВёрстка семантическая +20, \nВёрстка соответствует макету +48, \nТребования к css + 12, \nИнтерактивность, реализуемая через css +20');
