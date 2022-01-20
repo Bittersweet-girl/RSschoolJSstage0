2a69e0b03613595420bd08693bc4140c077f2840
Bittersweet-girl <enatav1@gmail.com>
2022-01-20 18:45:51 +0200

    feat: add burger-menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,11 @@
+const hamburger = document.querySelector('.header__burger');
+const headerNav = document.querySelector('.header__nav');
+
+function toggleMenu() {
+  hamburger.classList.toggle('open');
+  headerNav.classList.toggle('open');
+}
+hamburger.addEventListener('click', toggleMenu);
+headerNav.addEventListener('click', toggleMenu);
+
 console.log('Оценка 100. \nВёрстка валидная +10, \nВёрстка семантическая +20, \nВёрстка соответствует макету +48, \nТребования к css + 12, \nИнтерактивность, реализуемая через css +20');
