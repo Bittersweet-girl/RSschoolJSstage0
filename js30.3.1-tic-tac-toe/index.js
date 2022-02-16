26943c3d52f6fe509cafcccc95f1358d52d93bf6
Bittersweet-girl <enatav1@gmail.com>
2022-02-16 16:13:55 +0200

    feat: add title and add variables

--- js30.3.1-tic-tac-toe/index.js
+++ js30.3.1-tic-tac-toe/index.js
@@ -1,5 +1,11 @@
+document.querySelector('.buttons').addEventListener('click', (e) => {
+  document.body.style.backgroundColor = e.target.value;
+});
 
-  document.querySelector('.buttons').addEventListener('click', (e) => {
-    document.body.style.backgroundColor = e.target.value;
-  })
+const ceil = document.querySelectorAll(".field__item"),
+  text = document.querySelector(".main__title"),
+  resetGame = document.querySelector(".main__btn_reset"),
+  resetRes = document.querySelector(".main__btn_reset-res");
 
+var player = "X",
+  count = 0;
